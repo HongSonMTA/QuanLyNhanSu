58dd2515a95aebbb4167c43100e59bc2ec997665
HongSonMTA <36963575+HongSonMTA@users.noreply.github.com>
2018-03-26 22:06:27 +0700

    [Hồng Sơn ]  CSDL Và project Quản Lý Nhân Sự

---
+++ QuanLyNhanSu/QuanLyNhanSu_Entity/PhongBanEntity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu_Entity
+{
+    public class PhongBanEntity
+    {
+        String MaPB, TenPB, MaTP, DiaChi, Sdt;
+
+        public string Diachi
+        {
+            get
+            {
+                return DiaChi;
+            }
+
+            set
+            {
+                DiaChi = value;
+            }
+        }
+
+        public string Mapb
+        {
+            get
+            {
+                return MaPB;
+            }
+
+            set
+            {
+                MaPB = value;
+            }
+        }
+
+        public string MaTp
+        {
+            get
+            {
+                return MaTP;
+            }
+
+            set
+            {
+                MaTP = value;
+            }
+        }
+
+        public string SDT
+        {
+            get
+            {
+                return Sdt;
+            }
+
+            set
+            {
+                Sdt = value;
+            }
+        }
+
+        public string Tenpb
+        {
+            get
+            {
+                return TenPB;
+            }
+
+            set
+            {
+                TenPB = value;
+            }
+        }
+    }
+}
