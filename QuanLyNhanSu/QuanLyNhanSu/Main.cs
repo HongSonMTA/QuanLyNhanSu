7d9e770fc79ad7bd2ee741adb1023b979207b6bc
minhmeo0610 <36961484+minhmeo0610@users.noreply.github.com>
2018-03-28 20:33:26 +0700

    [minh] tạo from Main chưa có chức năng

---
+++ QuanLyNhanSu/QuanLyNhanSu/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThongTinCN_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
